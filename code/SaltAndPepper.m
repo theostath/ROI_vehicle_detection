61b11f2bc449e4cd8357c90ba753b01f16bc0200
theostath <theostath@users.noreply.github.com>
2022-06-02 21:50:17 +0300

    Update SaltAndPepper.m

--- code/SaltAndPepper.m
+++ code/SaltAndPepper.m
@@ -1,10 +1,10 @@
 function [image_impulse] = SaltAndPepper(image,p)
 
-% p = pososto kroustikou thorivou
+% p = percentage of impalse noise
 image_impulse = image;
 impulse = rand(size(image)); % random pixels [0:1]
-d = find(impulse < p/2);       % vriskw pixels me times < p/2  
+d = find(impulse < p/2);       % find pixels with value < p/2  
 image_impulse(d) = 0 ;      % minimum noise
-d = find(impulse >= p/2 & impulse < p); % vriskw pixels me times >= p/2 kai < p
+d = find(impulse >= p/2 & impulse < p); % find pixels with value >= p/2 and < p
 image_impulse(d) = 255;   % maximum noise
 end
