e83ddbcee234a34bebac3530555e83efb0ae9a5f
theostath <theostath@users.noreply.github.com>
2022-06-02 15:24:14 +0300

    Update FrameDifferencingMask.m

--- code/FrameDifferencingMask.m
+++ code/FrameDifferencingMask.m
@@ -1,7 +1,7 @@
 function [ROI_mask,frames_roi] = FrameDifferencingMask(frames,Threshold)
 
 %% RGB to Grayscale Frames
-gray_frames = zeros(480,704,300); % arxikopoihsh
+gray_frames = zeros(480,704,300); % initialize
 
 for i = 1:300
     gray_frames(:,:,i) = uint8(rgb2gray(frames(:,:,:,i)));
@@ -9,19 +9,19 @@ end
 
 %% Create ROI mask trhough Frame Differencing technique (Backgound Subtraction)
 
-ROI_mask = zeros(480,704,300); % arxikopoihsh
+ROI_mask = zeros(480,704,300); % initialize
 
 for i = 1:300
-    if i == 300 % eidikh periptwsh gia to teleutaio frame pou den exei epomeno
+    if i == 300 % special case fot the last frame
         ROI_mask(:,:,i) = ROI_Mask(gray_frames(:,:,i-1),gray_frames(:,:,i),Threshold);
     else
         ROI_mask(:,:,i) = ROI_Mask(gray_frames(:,:,i),gray_frames(:,:,i+1),Threshold);
     end
 end
 
-%% pointwise pollaplasiasmos ths maskas me ta arxika frames se RGB
+%% pointwise multiplication of the mask with the original frames in RGB
 
-frames_roi = frames; %arxikopoihsh
+frames_roi = frames; %initialize
 
 for i = 1:300
     frames_roi(:,:,:,i) = frames(:,:,:,i).*uint8(ROI_mask(:,:,i));
