a804e30b249adcbbcf00315a642492b1d72f39db
theostath <theostath@users.noreply.github.com>
2022-06-04 14:52:06 +0300

    Update MedianFilter.m

--- code/MedianFilter.m
+++ code/MedianFilter.m
@@ -1,9 +1,9 @@
 function [final_clock_median] = MedianFilter(clock)
 
 % zero padding
-[x, y] = size(clock); % diastasteis eikonas
+[x, y] = size(clock); % dimensions of the frame
 
-padd = floor(3/2); % opou 3 to megethos tou parathyrou
+padd = floor(3/2); % 3 is the size of the mask
 
 clock_median = zeros(x+2*padd,y+2*padd);
 
@@ -13,29 +13,29 @@ for i = padd+1:padd+x
     end
 end
 
-Med = zeros(1,9); % maska 3x3
+Med = zeros(1,9); % mask 3x3
 
-final_clock_median = clock; %arxikopoihsh
+final_clock_median = clock; % initialize
 
-% ypologismos timwn
+% calculating the values
 for i = padd+1:padd+x
     for j = padd+1:padd+y
-        % 1h grammh
+        % 1st row
         Med(1) = clock_median(i-1,j-1);
         Med(2) = clock_median(i-1,j);
         Med(3) = clock_median(i-1,j+1);
-        % 2h grammh
+        % 2nd row
         Med(4) = clock_median(i,j-1);
         Med(5) = clock_median(i,j);
         Med(6) = clock_median(i,j+1);
-        %3h grammh
+        % 3rd row
         Med(7) = clock_median(i+1,j-1);
         Med(8) = clock_median(i+1,j);
         Med(9) = clock_median(i+1,j+1);
         % the sorted neighbourhood matrix
         SortMed = sort(Med);
-        %the new intensity of pixel at (i,j) will be median of this
-        %matrix
+        % the new intensity of pixel at (i,j) will be median of this
+        % matrix
         final_clock_median(i-padd,j-padd) = SortMed(5);
     end
 end
